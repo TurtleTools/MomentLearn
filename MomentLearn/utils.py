3cfee45e159cae855ba655ec9edb4334c009c2cd
akdel <akdel.mehmet@gmail.com>
2021-11-03 22:29:44 +0100

    added notebook

--- MomentLearn/utils.py
+++ MomentLearn/utils.py
@@ -4,6 +4,8 @@ import prody as pd
 import numpy as np
 from caretta import superposition_functions
 from caretta import score_functions
+import umap
+import torch
 
 
 def get_all_kmer_moments_for_pdbs(pdbs: ty.List[ty.Tuple[str, str]], kmer_size: int=16,
@@ -34,3 +36,41 @@ def get_example_metadata():
         mapk_pdb_id_to_class[(pdb_id, chain)] = class_name
     X_names = list(mapk_pdb_id_to_class.keys())
     return X_names, mapk_pdb_id_to_class
+
+def get_embedding(prot_rep, bins):
+    return np.histogram(prot_rep, bins=bins)[0]
+
+
+def plot_umap(protein_moments,
+              model,
+              protein_labels):
+    all_moments = np.concatenate(protein_moments)
+    import matplotlib.pyplot as plt
+    ms = torch.tensor(all_moments.astype("float32"))
+    r, _, _ = model.forward(ms,ms,ms)
+    r = r.cpu().detach().numpy().T.flatten()
+    h = plt.hist(r)
+    start, end = h[1][0] , h[1][-1]
+    split = (h[1][-1] -  h[1][0])/100
+
+    ind_moments_compressed = [model.forward(torch.tensor(x.astype("float32")),
+                                            torch.tensor(x.astype("float32")),
+                                            torch.tensor(x.astype("float32")))[0].cpu().detach().numpy().T.flatten() for x in protein_moments]
+
+    protein_embeddings = [get_embedding(x, bins=np.arange(start, end, split)) for x in ind_moments_compressed]
+    protein_embeddings = [x/x.sum() for x in protein_embeddings]
+
+    reducer = umap.UMAP(metric="euclidean", n_components=2, n_neighbors=20)
+    reduced = reducer.fit_transform(protein_embeddings)
+
+    plt.figure(figsize=(10,10))
+    for i in range(3):
+        indices = np.where(np.array(protein_labels) == i)[0]
+        plt.scatter(reduced[indices, 0],
+                    reduced[indices, 1],
+                    edgecolor="black",
+                    linewidth=0.1,
+                    alpha=0.8)
+    plt.axis("off")
+    plt.legend()
+    plt.show()
